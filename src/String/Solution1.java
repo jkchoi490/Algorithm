12de78f42f7a97635fd5f2ca58de3112f027f74e
최정경 <jkchoi490@daum.net>
2022-11-07 19:31:01 +0900

    1. 문자 찾기

--- src/String/Solution1.java
+++ src/String/Solution1.java
@@ -1,9 +1,26 @@
 package String;
 
-public class Solution1 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-	public static void main(String[] args) {
-		
+public class Solution1 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
+		char c = br.readLine().charAt(0);
+		System.out.println(solution(str, c));
+	
+	}
+
+	private static int solution(String str, char c) {
+		int answer = 0;
+		str = str.toUpperCase(); 
+		c = Character.toUpperCase(c);
+		for(int i = 0; i<str.length(); i++) {
+			if(str.charAt(i) == c) answer++;
+		}
+		return answer;
 	}
 
 }
