4e9acf4274eb05415c6bf242537ee52129fcfa37
최정경 <jkchoi490@daum.net>
2022-11-28 18:44:08 +0900

    2022-11-28 12.암호

--- src/String/Solution12.java
+++ src/String/Solution12.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.StringTokenizer;
 
 public class Solution12 {
 
@@ -12,28 +11,67 @@ public class Solution12 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
 		String str = br.readLine();
-		int str_length = 7;
-		
-		for(int i = 0; i<str.length(); i++) {
-			String[] input = new String[str_length+1];
-			StringTokenizer st = new StringTokenizer(str);
-			for(int j = 0; j<input.length; j++) {
-				input[j] =  st.nextToken();
+		System.out.println(solution(n, str));
+	}
+
+	private static String solution(int n, String str) {
+		StringBuilder sb = new StringBuilder();
+		String[] div_arr = new String[n];
+	
+		for(int j = 0; j<4; j++) {
+			for(int i = 1; i<(7*n)+1; i++) {
+				sb.append(str.charAt(i-1)); 
+				//System.out.println(sb.toString());
+				if(i % 7 == 0) {
+					div_arr[j] = sb.toString();
+					//System.out.println(sb.toString());
+					sb.setLength(0); //StringBuilder 저장한 문자열 모두 제거
+					j++;
+					
+				}
+				
 			}
-		}
-		/*
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int str_length = 7;
-		String[] input = new String[str_length+1];
-		for(int i = 0; i<input.length; i++) {
-			input[i] =  st.nextToken();
+		//	System.out.println(Arrays.toString(div_arr));
 			
-		}
-		*/
+			String alphabet = "";
+			
+			for(int k = 0; k <div_arr.length; k++) {
+				alphabet = div_arr[k];
+				for(int a = 0 ; a<alphabet.length(); a++) {
+					
+					if(alphabet.charAt(a) == '#') {
+						alphabet = alphabet.replace('#', '1'); 
+						div_arr[k] = alphabet;
+					}
+					else if(alphabet.charAt(a) == '*') {
+						alphabet = alphabet.replace('*', '0');
+						div_arr[k] = alphabet;
+					}
+				}
+			}
+			//System.out.println(Arrays.toString(div_arr));
+					
+	}
+		StringBuilder s_r = new StringBuilder();
+		int ans = 0;
+		double result = 0;
 		
-		System.out.println(n);
-		System.out.println(str);
-		//System.out.println(Arrays.toString(input));
+		for(String s : div_arr) {
+			ans = 0;	
+			for(int i = 0; i<s.length(); i++) {
+				if(s.charAt(i) == '1') {
+					result = Math.pow(2, 6-i);
+					ans += result;
+				}
+				
+			}
+			
+			String a = Character.toString((char)ans);
+			s_r.append(a);
+		
+		}
+		
+		return s_r.toString();
 	}
 
 }
