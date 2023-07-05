d347bb3b77d51fca4d1d31d58c703ced3eeb65a8
최정경 <jkchoi490@daum.net>
2023-07-05 17:32:25 +0900

    0705 시간복잡도 공부

---
+++ src/TimeComplexity/BJ_24262알고리즘수업.java
@@ -0,0 +1,24 @@
+package TimeComplexity;
+
+public class BJ_24262알고리즘수업 {
+/*
+BAEKJOON ONLINE JUDGE 24262. 알고리즘 수업 - 알고리즘의 수행 시간 1
+
+MenOfPassion(A[], n) { 
+    i = ⌊n / 2⌋; 
+    return A[i]; # 코드1 -> 배열A의 i번째 원소 반환 -> O(1)
+}
+
+입력 :  n(1 ≤ n ≤ 500,000)
+시간복잡도 : O(1)
+
+*/
+	public static void main(String[] args) {
+		
+		System.out.println(1); // 코드 1의 수행횟수
+		System.out.println(0); // 코드1의 수행 횟수를 다항식으로 나타내었을 때, 최고차항의 차수 -> 시간복잡도가 O(1)이기 때문에 최고차항의 차수는 0
+		
+
+	}
+
+}
