38176afe03f0f83f74d74ca2e6b024d58de1ad83
jkchoi490 <jkchoi490@daum.net>
2023-08-12 18:52:22 +0900

    2023-08-12 DFS 공부 - Tree 말단노드까지의 까장 짧은 경로(DFS)

--- src/Recursive_Tree_Graph/Solution_DFS1.java
+++ src/Recursive_Tree_Graph/Solution_DFS1.java
@@ -1,17 +1,17 @@
 package Recursive_Tree_Graph;
 
-class Node{
+class Node1{
 	int data;
-	Node lt, rt;
-	public Node(int val) {
+	Node1 lt, rt;
+	public Node1(int val) {
 		data = val;
 		lt=rt=null;
 	}
 }
 public class Solution_DFS1 { // Tree 말단노드까지의 까장 짧은 경로(DFS)
 	
-	Node root;
-	public int DFS(int L, Node root) {
+	Node1 root;
+	public int DFS(int L, Node1 root) {
 		if(root.lt == null && root.rt == null) {//말단 노드인 경우
 			return L; //L값 리턴
 		}else { //말단노드가 아닐경우
@@ -24,11 +24,11 @@ public class Solution_DFS1 { // Tree 말단노드까지의 까장 짧은 경로(
 		
 		//트리 생성
 		Solution_DFS1 tree = new Solution_DFS1();
-		tree.root=new Node(1); 
-        tree.root.lt=new Node(2); 
-        tree.root.rt=new Node(3); 
-        tree.root.lt.lt=new Node(4); 
-        tree.root.lt.rt=new Node(5); 
+		tree.root=new Node1(1); 
+        tree.root.lt=new Node1(2); 
+        tree.root.rt=new Node1(3); 
+        tree.root.lt.lt=new Node1(4); 
+        tree.root.lt.rt=new Node1(5); 
         System.out.println(tree.DFS(0, tree.root)); 
 
 	}
