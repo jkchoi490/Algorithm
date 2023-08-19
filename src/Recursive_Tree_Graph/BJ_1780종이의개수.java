ddfc9f480594fcc10f6bca3739da5e55e16bb212
jeonggyeong Choi <60879018+jkchoi490@users.noreply.github.com>
2023-08-19 23:45:07 +0900

    2023-08-19 DFS 공부 - 종이의개수
    DFS공부

---
+++ src/Recursive_Tree_Graph/BJ_1780종이의개수.java
@@ -0,0 +1,57 @@
+package Recursive_Tree_Graph;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class BJ_1780종이의개수 { //분할정복문제
+	
+	static int N;
+	static int[][] graph;
+	static int[] count;
+
+	private static void divide(int x, int y, int n) {
+		if(check(x, y, n)) {
+			count[graph[x][y]+1]++;
+		}else {
+			int s = n/3;
+			for(int i = 0; i<3; i++) {
+				for(int j = 0; j<3; j++) {
+					divide(x+s*i, y+s*j, s);
+				}
+			}
+		}	
+	}
+	
+	private static boolean check(int x, int y, int n) {
+		int now = graph[x][y];
+		for(int i = x; i<x+n; i++) {
+			for(int j = y; j<y+n; j++) {
+				if(now !=graph[i][j]) return false;
+			}	
+		}
+		return true;
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		graph = new int[N][N];
+		count = new int[3];
+		StringTokenizer st;
+		for(int i = 0; i<N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j = 0; j<N; j++) {
+				graph[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		divide(0,0,N);
+		System.out.println(count[0]);
+		System.out.println(count[1]);
+		System.out.println(count[2]);
+	}
+
+}
