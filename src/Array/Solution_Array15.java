fc2a549dbe0ee8b5cfad6591c0b397bdd76bcc8c
jeonggyeong Choi <60879018+jkchoi490@users.noreply.github.com>
2023-09-08 21:50:13 +0900

    2023-09-08 배열 공부 - 프로그래밍 대회
    2023-09-08 배열 공부 - 프로그래밍 대회

---
+++ src/Array/Solution_Array15.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Solution_Array15 { // 프로그래밍 대회
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st;
+		
+		ArrayList<Integer> submit = new ArrayList<>();
+		ArrayList<Integer> time = new ArrayList<>();
+		
+		for(int i = 0; i<N; i++) {
+			int result = 0;
+			st = new StringTokenizer(br.readLine());
+			String name = st.nextToken();
+			
+		}
+	}
+}
