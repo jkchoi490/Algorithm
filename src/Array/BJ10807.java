e429c696d9aba0b21519ee091318731ea3540c3e
jeonggyeong Choi <60879018+jkchoi490@users.noreply.github.com>
2023-09-10 23:45:03 +0900

    backjoon

---
+++ src/Array/BJ10807.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ10807 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[] arr = new int[N];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i = 0; i<N; i++) arr[i] = Integer.parseInt(st.nextToken());
+		int v = Integer.parseInt(br.readLine());
+		int cnt = 0;
+		for(int x : arr) {
+			if(x == v) cnt++;
+		}
+		System.out.println(cnt);
+	}
+
+}
