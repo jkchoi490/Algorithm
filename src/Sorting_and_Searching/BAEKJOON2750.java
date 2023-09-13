e1a7c340d2c8b1ad305a10061a04a3f3afc1c525
jeonggyeong Choi <60879018+jkchoi490@users.noreply.github.com>
2023-09-13 21:50:16 +0900

    baekjoon

---
+++ src/Sorting_and_Searching/BAEKJOON2750.java
@@ -0,0 +1,29 @@
+package Sorting_and_Searching;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BAEKJOON2750 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n= Integer.parseInt(br.readLine());
+		int[] arr = new int[n];
+		for(int i = 0; i<n; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		for(int i = 0; i<n; i++) {
+			for(int j = i+1; j<n; j++) {
+				if(arr[i]>arr[j]) {
+				int tmp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = tmp;
+				}
+			}
+		}
+		
+		for(int i = 0; i<n; i++) System.out.println(arr[i]);
+	}
+
+}
