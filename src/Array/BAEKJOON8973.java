73aecd20a241bdbee258812d1316349dc2380c6f
jeonggyeong Choi <60879018+jkchoi490@users.noreply.github.com>
2023-09-16 20:00:11 +0900

    baekjoon

---
+++ src/Array/BAEKJOON8973.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BAEKJOON8973 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int[] arr = new int[n];
+		int[] arr1 = new int[n];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i = 0; i<n; i++) arr[i] = Integer.parseInt(st.nextToken());
+		for(int i = 0; i<n; i++) arr1[i] = Integer.parseInt(st.nextToken());
+		
+		int b = 0;
+		
+		
+	}
+
+}
