b9e02c1401be3e04c16824da014b9e349a889e36
jeonggyeong Choi <60879018+jkchoi490@users.noreply.github.com>
2023-09-17 23:00:23 +0900

    baekjoon

---
+++ src/Array/BAEKJOON25020.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Vector;
+
+public class BAEKJOON25020 {
+
+	public static void main(String[] args) throws IOException {
+		int n = 0;
+		int m = 0;
+		Vector<Integer> vectorA = new Vector<Integer>(n);
+		Vector<Integer> vectorB = new Vector<Integer>(n-1);
+		Vector<Integer> vectorL = new Vector<Integer>(m);
+		Vector<Integer> vectorU = new Vector<Integer>(m);
+		//testset(vector);
+	}
+
+	private static void testset(Vector<Integer> A, Vector<Integer> B,Vector<Integer> L,Vector<Integer> U) {
+	
+		
+	}
+
+	
+	
+}
