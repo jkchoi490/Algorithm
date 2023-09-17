de54918d3d22830a19ef8ca84516458aef3ae619
jeonggyeong Choi <60879018+jkchoi490@users.noreply.github.com>
2023-09-17 23:00:13 +0900

    baekjoon

---
+++ src/String/BAEKJOON7607.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BAEKJOON7607 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		String str = br.readLine(); 
+		//slicing(str,b,e);
+
+	}
+
+	private static String slicing(String str, int begin, int end) {
+		
+		
+		
+		return str;
+	}
+
+}
