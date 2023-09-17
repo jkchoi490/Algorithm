609f1e12fcca60f9956e2eaf034c6fe55d9354ce
jeonggyeong Choi <60879018+jkchoi490@users.noreply.github.com>
2023-09-17 23:00:15 +0900

    baekjoon

---
+++ src/String/BAEKJOON3081.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BAEKJOON3081 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		StringTokenizer st= new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int e = Integer.parseInt(st.nextToken());
+		for(int i = 0; i<e; i++) {
+			st= new StringTokenizer(br.readLine());
+			int command = Integer.parseInt(st.nextToken());
+			
+		}
+	}
+
+}
