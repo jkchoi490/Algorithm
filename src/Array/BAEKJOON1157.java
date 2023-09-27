28d19535c4de1022c36cafc58e59482e68a306e8
jeonggyeong Choi <60879018+jkchoi490@users.noreply.github.com>
2023-09-27 23:57:30 +0900

    baekjoon

--- src/Array/BAEKJOON1157.java
+++ src/Array/BAEKJOON1157.java
@@ -1,12 +1,14 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
 
 public class BAEKJOON1157 {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String word = br.readLine();
+		int[] arr = new int[word.length()];
 		for(char w :word.toCharArray()) {
 			
 		}
