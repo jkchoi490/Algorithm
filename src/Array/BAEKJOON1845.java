fb9b4ff2250f283842b54956b4fb587b7049192f
jeonggyeong Choi <60879018+jkchoi490@users.noreply.github.com>
2023-10-03 23:58:03 +0900

    baekjoon
    다시풀기

--- src/Array/BAEKJOON1845.java
+++ src/Array/BAEKJOON1845.java
@@ -1,18 +1,8 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
-
-
 public class BAEKJOON1845 {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		int[] arr = new int[N];
-		for(int i = 1; i<=N; i++) 
-			arr[i-1] = i;
-	}
 	
-		
+	
+	
+	
+	
+	
 }
