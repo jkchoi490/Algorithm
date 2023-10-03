bff612d9d2a59f502470571761e12c97ba4acfce
jeonggyeong Choi <60879018+jkchoi490@users.noreply.github.com>
2023-10-03 23:38:04 +0900

    baekjoon

---
+++ src/Array/BAEKJOON1605.java
@@ -0,0 +1,18 @@
+import java.io.*;
+
+
+
+public class BAEKJOON1605 {
+
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int L = Integer.parseInt(br.readLine());
+		char[] arr = br.readLine().toCharArray();	
+	}
+
+
+	
+
+
+}
