e5b10ebf7fbfe986ba08cc699401f7df131b4d74
jeonggyeong Choi <60879018+jkchoi490@users.noreply.github.com>
2023-10-06 23:57:34 +0900

    baekjoon

--- src/Array/BAEKJOON1187.java
+++ src/Array/BAEKJOON1187.java
@@ -2,19 +2,28 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
+class Num{
+	
+}
 public class BAEKJOON1187 {
-
 	public static void main(String[] args) throws IOException {
+
+	
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	    int n = Integer.parseInt(br.readLine());
 	    int[] arr = new int[(2*n)-1];
+	    Deque<Num> dq = new ArrayDeque<>();
 	    StringTokenizer st = new StringTokenizer(br.readLine());
 	    for(int i = 0; i<(2*n-1); i++) arr[i] = Integer.parseInt(st.nextToken());
 	    int k = 1;
 	    int tmp = n;
 	    while((tmp>>=1) != 1) {
 	    	k++;
-	    }	    
+	    }
+
 	}
+	
 }
