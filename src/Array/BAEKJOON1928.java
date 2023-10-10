e6190feed0194a157b70b8e218fd2838ec1d1bed
jeonggyeong Choi <60879018+jkchoi490@users.noreply.github.com>
2023-10-10 23:45:06 +0900

    baekjoon

---
+++ src/Array/BAEKJOON1928.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class BAEKJOON1928 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+	}
+
+
+}
