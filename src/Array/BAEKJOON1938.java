b8db0efab418b1597eb90b17a636e1883f7db6aa
jeonggyeong Choi <60879018+jkchoi490@users.noreply.github.com>
2023-10-13 12:10:03 +0900

    baekjoon

---
+++ src/Array/BAEKJOON1938.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BAEKJOON1938 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[][] arr = new int[N][N];
+		}
+}
