2582805124e2882f59ac28da3e858e4762e238de
jeonggyeong Choi <60879018+jkchoi490@users.noreply.github.com>
2023-10-14 23:49:02 +0900

    baekjoon

--- src/Recursive_Tree_Graph/BAEKJOON1849.java
+++ src/Recursive_Tree_Graph/BAEKJOON1849.java
@@ -1,9 +1,18 @@
+package 다시공부;
+
+
+import java.io.*;
+
 public class BAEKJOON1849 { 
 	
-	
 	public static void main(String[] args) throws Exception {
-	
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
 		
 	
 	}
+		
+		
+	
+
 }
