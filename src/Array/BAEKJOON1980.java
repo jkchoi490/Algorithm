f51e3f0561bd5c7493f5bddb56372454d58f424e
jeonggyeong Choi <60879018+jkchoi490@users.noreply.github.com>
2023-10-15 23:08:04 +0900

    baekjoon

---
+++ src/Array/BAEKJOON1980.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class BAEKJOON1980 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		
+	}
+}
