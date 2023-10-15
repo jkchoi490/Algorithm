1756eb8f7ea5ff7a585e2e9f52a7817add13a72a
jeonggyeong Choi <60879018+jkchoi490@users.noreply.github.com>
2023-10-16 01:00:25 +0900

    baekjoon

--- src/Array/BAEKJOON1843.java
+++ src/Array/BAEKJOON1843.java
@@ -2,14 +2,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+
+
+
 public class BAEKJOON1843 {
-
-	
-	public static void main(String[] args) throws IOException {
-		
-		}
-
-	
-	
 
 }
