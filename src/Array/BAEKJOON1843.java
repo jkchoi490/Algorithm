b7b9154fd821adc62f6fa738c87d261b7b6c39ac
jeonggyeong Choi <60879018+jkchoi490@users.noreply.github.com>
2023-10-16 00:30:41 +0900

    baekjoon

--- src/Array/BAEKJOON1843.java
+++ src/Array/BAEKJOON1843.java
@@ -4,9 +4,12 @@ import java.io.InputStreamReader;
 
 public class BAEKJOON1843 {
 
+	
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
 		
 		}
+
+	
+	
+
 }
