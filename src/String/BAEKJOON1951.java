4f85b07222b0e950be07f69438a09bc491f6ff33
jeonggyeong Choi <60879018+jkchoi490@users.noreply.github.com>
2023-10-17 00:35:10 +0900

    Update BAEKJOON1951.java

--- src/String/BAEKJOON1951.java
+++ src/String/BAEKJOON1951.java
@@ -1,7 +1,7 @@
 import java.io.*;
 public class BAEKJOON1951 {
-
 	public static void main(String[] args) {		
+		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));} 		
 	
 	
