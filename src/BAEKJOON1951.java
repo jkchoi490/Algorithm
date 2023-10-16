724d8c7efb4529b0c82a3472e0e883c60953cdf0
jkchoi490 <jkchoi490@daum.net>
2023-10-16 11:59:00 +0900

    baekjoon

---
+++ src/BAEKJOON1951.java
@@ -0,0 +1,9 @@
+import java.io.*;
+public class BAEKJOON1951 {
+	public static void main(String[] args) {		
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));} 		
+
+
+
+}
