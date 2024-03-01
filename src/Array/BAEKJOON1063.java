f44b1400d6f5001a071847b1e7a7ddf0e49ecf84
jeonggyeong Choi <60879018+jkchoi490@users.noreply.github.com>
2024-03-01 23:57:51 +0900

    baekjoon

--- src/Array/BAEKJOON1063.java
+++ src/Array/BAEKJOON1063.java
@@ -8,6 +8,7 @@ public class BAEKJOON1063 {
 	static String[] dir = {"R","L","B","T","RT","LT","RB","LB"};
 	static int dx[] = {0, 0, 1,-1,-1,-1,1,1};
 	static int dy[] = {1, -1,0,0,1,-1,1,-1};
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
