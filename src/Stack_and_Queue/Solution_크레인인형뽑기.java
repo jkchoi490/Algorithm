1618bd2037c7302efd40050c658aa9fac93259a5
jeonggyeong Choi <60879018+jkchoi490@users.noreply.github.com>
2024-08-07 23:34:15 +0900

    programmers

---
+++ src/Stack_and_Queue/Solution_크레인인형뽑기.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution_크레인인형뽑기 {
+    public int solution(int[][] board, int[] moves) {
+        int answer = 0;
+        Stack<Integer> stack = new Stack<>();
+        for(int move : moves){
+            for(int i = 0; i<board.length; i++){
+                if(board[i][move-1] > 0) {
+                    if(!stack.isEmpty() && board[i][move-1]==stack.peek()){
+                        answer+=2;
+                        stack.pop();
+                    }
+                    else stack.push(board[i][move-1]);
+                    board[i][move-1] = 0;
+                    break;
+                }
+            }
+        }
+
+        
+        return answer;
+    }
+}
