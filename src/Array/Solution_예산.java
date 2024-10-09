4686b9836316e9dbadfebc1a2c5231fbc92c38f9
jeonggyeong Choi <60879018+jkchoi490@users.noreply.github.com>
2024-10-09 23:00:50 +0900

    programmers

---
+++ src/Array/Solution_예산.java
@@ -0,0 +1,19 @@
+package DAY1009;
+
+import java.util.*;
+
+class Solution_예산 {
+   
+    public int solution(int[] d, int budget) {
+        int answer = 0;
+        Arrays.sort(d);
+        for(int i = 0; i<d.length; i++){
+            if(budget >= d[i]){
+                budget -= d[i];
+                answer++;   
+            }
+        }
+        return answer;
+    }
+  
+}
