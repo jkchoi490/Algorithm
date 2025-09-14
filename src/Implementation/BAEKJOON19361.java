e7bb6ac4d0117a6a669bfde79f807a8e11bcdad5
jkchoi490 <jkchoi490@daum.net>
2025-09-14 21:11:32 +0900

    BAEKJOON19361 - Wrong Answer

---
+++ src/Implementation/BAEKJOON19361.java
@@ -0,0 +1,15 @@
+package Implementation;
+
+//Wrong Answer
+public class BAEKJOON19361 {
+    public static void main(String[] args) {
+
+        int N = 5;
+        System.out.println(N);
+
+        System.out.println("1 2 3 4");
+        System.out.println("10 9 8");
+        System.out.println("7 6");
+        System.out.println("5");
+    }
+}
