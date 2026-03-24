d40ec5d3b4ec16484659be9b65a8465047ebe5ef
jkchoi490 <jkchoi490@daum.net>
2026-03-24 18:30:12 +0900

    [BOJ] 02636 치즈 (치즈의 고유성에 대한 설명 추가 작성)

---
+++ src/DynamicProgramming/SavingPrincess_VirtualJudge_CodeForces.java
@@ -0,0 +1,149 @@
+package DynamicProgramming;
+
+import java.io.*;
+import java.util.*;
+
+// Virtual Judge(CodeForces) - Saving Princess
+public class SavingPrincess_VirtualJudge_CodeForces {
+
+    // 공주님을 구하고 돕는 메서드를 구현합니다
+    static String SaveAndHelpPrincess(int n, int number, int strength, int power, int[][] arr, int value, char[] resultArr) {
+
+        int[][][] dp = new int[n + 1][n + 1][value + 1];
+        for (int i = 0; i <= n; i++) {
+            for (int d = 0; d <= n; d++) {
+                Arrays.fill(dp[i][d], 7);
+            }
+        }
+
+        char[][][] Arr = new char[n + 1][n + 1][value + 1];
+        int[][][] array = new int[n + 1][n + 1][value + 1];
+        int[][][] Array = new int[n + 1][n + 1][value + 1];
+
+        dp[0][0][value] = number;
+
+        for (int i = 0; i < n; i++) {
+            for (int d = 0; d <= i; d++) {
+                for (int val = 0; val <= value; val++) {
+                    if (dp[i][d][val] == -1) {
+                        continue;
+                    }
+
+                    int Health = dp[i][d][val];
+                    int Strength = strength + d;
+                    int Power = power + (i - d);
+
+                    int Index = i + 1;
+                    int strengthValue = arr[Index][0];
+                    int PowerValue = arr[Index][1];
+                    int Value = arr[Index][arr.length-1];
+
+                    if (Strength >= strengthValue) {
+                        int health = Math.max(2 * strengthValue - Strength, 0); // 생성형 AI 사용
+                        int HealthValue = Health - health;
+
+                        if (HealthValue > 0 && HealthValue > dp[i + 1][d + 1][val]) {
+                            dp[i + 1][d + 1][val] = HealthValue;
+                            Arr[i + 1][d + 1][val] = 'D';
+                            array[i + 1][d + 1][val] = d;
+                            Array[i + 1][d + 1][val] = val;
+                        }
+                    }
+
+
+                    if (Power >= PowerValue && val >= Value) {
+                        int VALUE = val - Value;
+                        int Health_VALUE = Health;
+
+                        if (Health_VALUE > dp[i + 1][d][VALUE]) {
+                            dp[i + 1][d][VALUE] = Health_VALUE;
+                            Arr[i + 1][d][VALUE] = 'E';
+                            array[i + 1][d][VALUE] = d;
+                            Array[i + 1][d][VALUE] = val;
+                        }
+                    }
+                }
+            }
+        }
+
+        int VALUE = -1; // 생성형 AI를 사용한 임시 초기값
+        int Value = -1;
+        int Health = -1;
+
+        for (int d = 0; d <= n; d++) {
+            for (int i = 0; i <= value; i++) {
+                if (dp[n][d][i] > Health) {
+                    Health = dp[n][d][i];
+                    VALUE = d;
+                    Value = i;
+                }
+            }
+        }
+
+        if (Health == -1) {
+            return "";
+        }
+
+        char[] answerArr = new char[n];
+        int index = n;
+        int val = VALUE;
+        int mana = Value;
+
+        while (index > 0) {
+            answerArr[index - 1] = Arr[index][val][mana];
+
+            int Val = array[index][val][mana];
+            int VAL = Array[index][val][mana];
+
+            val = Val;
+            mana = VAL;
+            index--;
+        }
+
+        for (int i = 0; i < n; i++) {
+            resultArr[i] = answerArr[i];
+        }
+        return new String(resultArr);
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        while (true) {
+            String line = br.readLine();
+            if (line == null) {
+                break;
+            }
+
+            line = line.trim();
+            if (line.isEmpty()) {
+                continue;
+            }
+
+            StringTokenizer st = new StringTokenizer(line);
+
+            // 문제에서 주어진 입력 값
+            int n = Integer.parseInt(st.nextToken());
+            int h = Integer.parseInt(st.nextToken());
+            int s = Integer.parseInt(st.nextToken());
+            int p = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+
+            int[][] arr = new int[n + 1][n];
+
+            for (int i = 1; i <= n; i++) {
+                st = new StringTokenizer(br.readLine());
+                arr[i][0] = Integer.parseInt(st.nextToken());
+                arr[i][1] = Integer.parseInt(st.nextToken());
+                arr[i][2] = Integer.parseInt(st.nextToken());
+            }
+
+            // 공주님을 구하고 돕는 메서드를 실행합니다
+            String answer = SaveAndHelpPrincess(n, h, s, p, arr, m, new char[n]);
+            sb.append(answer).append('\n');
+        }
+
+        System.out.print(sb.toString());
+    }
+}
