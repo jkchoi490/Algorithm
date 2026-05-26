e857de0226372b2fa420020428e196220eab1cdd
jkchoi490 <jkchoi490@daum.net>
2026-05-26 22:05:23 +0900

    SaveAcademy - Fast Independent Component Analysis (생성자 코드 설명 추가)

--- src/Implementation/FastICA_SaveAcademy.java
+++ src/Implementation/FastICA_SaveAcademy.java
@@ -44,11 +44,17 @@ public class FastICA_SaveAcademy {
                               double independentComponent,
                               long independentSeed,
                               double[] independentAverageArr) {
+        // 추출할 독립 성분 개수 저장
         this.independentComponentCount = independentComponentCount;
+        // 최대 반복 횟수 저장
         this.independentMaxIter = independentMaxIter;
+        // 학습률 저장
         this.independentRate = independentRate;
+        // 독립 성분 관련 파라미터 저장
         this.independentComponent = independentComponent;
+        // Random 객체 생성
         this.independentRandom = new Random(independentSeed);
+        // 평균 배열 저장
         this.independentAverageArr = independentAverageArr;
     }
 
