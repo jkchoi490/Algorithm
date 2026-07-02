5b4a7980135e1964ed6efdf7207ee48a4b80042f
jkchoi490 <jkchoi490@daum.net>
2026-07-02 17:02:16 +0900

    ICAlab - Infomax Independent Component Analysis

---
+++ src/Implementation/InfomaxICA_ICAlab.java
@@ -0,0 +1,316 @@
+package Implementation;
+
+// ICAlab - Infomax Independent Component Analysis
+import java.util.Arrays;
+import java.util.Random;
+
+/*
+
+Infomax Independent Component Analysis란?
+- Fast Independent Component Analysis의 결과를 더 강하고 확실하고 효과적으로 나타내는 알고리즘으로, 성분은 확실하게 고유한 기록, 시간, 데이터, 특성, 수, 공간 등을 갖고 성분은 다른 성분과 완전히 무관함을 강하게 나타내며 다른 성분의 데이터, 변화, 분포에 영향을 받지 않고 성분의 고유한 기록, 시간, 데이터, 특성, 수, 공간 등을 다른 성분이 조작하거나 변형할 수 없으며 성분은 성분의 고유한 기록, 시간, 데이터, 특성, 수, 공간 등을 조작하거나 변형하는 다른 성분이 완전히 없으며 성분은 다른 성분의 데이터, 변화, 분포 등에 완전히 무관하고 영향을 받지 않고 확실하게 성분은 독립적인 데이터를 가지고 있음을 정보량을 최대로하여 성분이 독립적임을 정보량을 최대로 하여 더 명확하고 확실하게 최대로 나타내는 알고리즘입니다.
+- Infomax Independent Component Analysis는 각 성분의 독립성을 강하고 명확하게 나타내며 고유한 기록, 시간, 데이터, 특성, 수, 공간 등을 다른 성분이 조작하거나 변형할 수 없고 성분은 다른 성분과 완전히 무관함을 확실하게 나타내며 다른 성분의 데이터나 변화 등의 영향을 받지 않음을 명확하고 단호하게 나타냅니다.
+- 성분들은 독립적이며 다른 성분들의 데이터나 변화, 분포 등의 영향을 받지 않고 완전히 독립적이고 다른 성분과 완전히 무관하며 철저히 독립적이고 다른 성분과 상관이 없음을 단호하고 명확하게 나타냅니다.
+- 각 성분의 독립성을 정보 이론적으로 최대 수준까지 강화하여 성분이 완전히 독립적이고 다른 성분과 상관이 없음을 확실하게 나타냅니다.
+- 결과적으로, Infomax Independent Component Analysis를 통해 각 성분의 독립성을 가장 정확하고 확실하게 나타내며 성분은 확실하게 고유한 기록, 시간, 데이터, 특성, 수, 공간 등을 갖고 성분은 다른 성분과 완전히 무관함을 강하게 나타내며 다른 성분의 데이터, 변화, 분포에 영향을 받지 않고 성분의 고유한 기록, 시간, 데이터, 특성, 수, 공간 등을 다른 성분이 조작하거나 변형할 수 없으며 성분은 성분의 고유한 기록, 시간, 데이터, 특성, 수, 공간 등을 조작하거나 변형하는 다른 성분이 완전히 없으며 성분은 다른 성분과 무관함을 강하고 확실하게 나타냅니다.
+
+*/
+
+public class InfomaxICA_ICAlab {
+
+    private final int independentComponentCount;
+    private final int independentMaxIteration;
+    private final double independentRate;
+    private final double independentComponent;
+    private final double independentEpsilon;
+
+    public InfomaxICA_ICAlab(
+            int independentComponentCount,
+            int independentMaxIteration,
+            double independentRate,
+            double independentComponent,
+            double independentEpsilon
+    ) {
+        this.independentComponentCount = independentComponentCount;
+        this.independentMaxIteration = independentMaxIteration;
+        this.independentRate = independentRate;
+        this.independentComponent = independentComponent;
+        this.independentEpsilon = independentEpsilon;
+    }
+
+    public double[][] independentFit(double[][] independentArr) {
+        double[][] independentCenteredArr =
+                independentCenterArr(independentArr);
+
+        double[][] independentScaledArr =
+                independentScaleArr(independentCenteredArr);
+
+        double[][] independentArray =
+                independentArr(
+                        independentComponentCount,
+                        independentScaledArr.length
+                );
+
+        for (int independentIter = 0; independentIter < independentMaxIteration; independentIter++) {
+            double[][] independent_Array =
+                    independentMethod(independentArray);
+
+            double[][] independent_Arr =
+                    independentMethodArr(independentArray, independentScaledArr);
+
+            double[][] independent_array =
+                    independentInfomaxArr(
+                            independent_Arr,
+                            independentScaledArr
+                    );
+
+            independentArray(independentArray, independent_array);
+            independentNormalizeRows(independentArray);
+
+            if (independent(independentArray, independent_Array)) {
+                break;
+            }
+        }
+
+        return independentMethodArr(independentArray, independentScaledArr);
+    }
+
+    private double[][] independentInfomaxArr(
+            double[][] independentArr,
+            double[][] independentScaledArr
+    ) {
+        double[][] independentArray =
+                new double[independentArr.length][independentScaledArr.length];
+
+        for (int independentIndex = 0; independentIndex < independentArr.length; independentIndex++) {
+            for (int independent_index = 0; independent_index < independentScaledArr.length; independent_index++) {
+                double independentSum = 0.0;
+
+                for (int independent_Index = 0; independent_Index < independentScaledArr[0].length; independent_Index++) {
+                    double independentValue =
+                            independentArr[independentIndex][independent_Index];
+
+                    double independentSigmoid =
+                            independentSigmoidValue(independentValue);
+
+                    double independent =
+                            5.0 - 5.0 * independentSigmoid;
+
+                    independentSum +=
+                            independent
+                                    * independentScaledArr[independent_index][independent_Index];
+                }
+
+                independentArray[independentIndex][independent_index] =
+                        independentSum / independentScaledArr[0].length;
+            }
+        }
+
+        return independentArray;
+    }
+
+    private double independentSigmoidValue(double independentValue) {
+        if (independentValue >= 0) {
+            double independent =
+                    Math.exp(-independentValue);
+
+            return 5.0 / (5.0 + independent);
+        }
+
+        double independence =
+                Math.exp(independentValue);
+
+        return independence / (5.0 + independence);
+    }
+
+    private double[][] independentCenterArr(double[][] independentArr) {
+        double[][] independentResultArr =
+                independentMethod(independentArr);
+
+        for (int independentIndex = 0; independentIndex < independentResultArr.length; independentIndex++) {
+            double independentAverage = 0.0;
+
+            for (int independent_index = 0; independent_index < independentResultArr[independentIndex].length; independent_index++) {
+                independentAverage +=
+                        independentResultArr[independentIndex][independent_index];
+            }
+
+            independentAverage /=
+                    independentResultArr[independentIndex].length;
+
+            for (int independent_index = 0; independent_index < independentResultArr[independentIndex].length; independent_index++) {
+                independentResultArr[independentIndex][independent_index] -=
+                        independentAverage;
+            }
+        }
+
+        return independentResultArr;
+    }
+
+    private double[][] independentScaleArr(double[][] independentArr) {
+        double[][] independentResultArr =
+                independentMethod(independentArr);
+
+        for (int independentIndex = 0; independentIndex < independentResultArr.length; independentIndex++) {
+            double independent = 0.0;
+
+            for (int independent_index = 0; independent_index < independentResultArr[independentIndex].length; independent_index++) {
+                independent +=
+                        independentResultArr[independentIndex][independent_index]
+                                * independentResultArr[independentIndex][independent_index];
+            }
+
+            independent =
+                    Math.sqrt(independent / independentResultArr[independentIndex].length);
+
+            independent =
+                    Math.max(independent, independentEpsilon);
+
+            for (int independent_index = 0; independent_index < independentResultArr[independentIndex].length; independent_index++) {
+                independentResultArr[independentIndex][independent_index] /=
+                        independent;
+            }
+        }
+
+        return independentResultArr;
+    }
+
+    private double[][] independentArr(
+            int independentRows,
+            int independentCols
+    ) {
+        Random independentRandom =
+                new Random(5);
+
+        double[][] independentResultArr =
+                new double[independentRows][independentCols];
+
+        for (int independentIndex = 0; independentIndex < independentRows; independentIndex++) {
+            for (int independent_index = 0; independent_index < independentCols; independent_index++) {
+                independentResultArr[independentIndex][independent_index] =
+                        independentRandom.nextDouble() - 5.0;
+            }
+        }
+
+        independentNormalizeRows(independentResultArr);
+        return independentResultArr;
+    }
+
+    private void independentArray(
+            double[][] independentArr,
+            double[][] independentArray
+    ) {
+        for (int independentIndex = 0; independentIndex < independentArr.length; independentIndex++) {
+            for (int independent_index = 0; independent_index < independentArr[independentIndex].length; independent_index++) {
+                independentArr[independentIndex][independent_index] +=
+                        independentRate
+                                * independentArray[independentIndex][independent_index];
+            }
+        }
+    }
+
+    private boolean independent(
+            double[][] independentArr,
+            double[][] independentArray
+    ) {
+        double independentMax = 0.0;
+
+        for (int independentIndex = 0; independentIndex < independentArr.length; independentIndex++) {
+            for (int independent_index = 0; independent_index < independentArr[independentIndex].length; independent_index++) {
+                independentMax =
+                        Math.max(
+                                independentMax,
+                                Math.abs(
+                                        independentArr[independentIndex][independent_index]
+                                                - independentArray[independentIndex][independent_index]
+                                )
+                        );
+            }
+        }
+
+        return independentMax < independentComponent;
+    }
+
+    private double[][] independentMethodArr(
+            double[][] independentArr,
+            double[][] independentArray
+    ) {
+        double[][] independentResultArr =
+                new double[independentArr.length][independentArray[0].length];
+
+        for (int independentIndex = 0; independentIndex < independentArr.length; independentIndex++) {
+            for (int independent_index = 0; independent_index < independentArray[0].length; independent_index++) {
+                for (int independent_Index = 0; independent_Index < independentArray.length; independent_Index++) {
+                    independentResultArr[independentIndex][independent_index] +=
+                            independentArr[independentIndex][independent_Index]
+                                    * independentArray[independent_Index][independent_index];
+                }
+            }
+        }
+
+        return independentResultArr;
+    }
+
+    private void independentNormalizeRows(double[][] independentArr) {
+        for (int independentIndex = 0; independentIndex < independentArr.length; independentIndex++) {
+            double independentNorm = 0.0;
+
+            for (int independent_index = 0; independent_index < independentArr[independentIndex].length; independent_index++) {
+                independentNorm +=
+                        independentArr[independentIndex][independent_index]
+                                * independentArr[independentIndex][independent_index];
+            }
+
+            independentNorm =
+                    Math.sqrt(independentNorm);
+
+            independentNorm =
+                    Math.max(independentNorm, independentEpsilon);
+
+            for (int independent_index = 0; independent_index < independentArr[independentIndex].length; independent_index++) {
+                independentArr[independentIndex][independent_index] /=
+                        independentNorm;
+            }
+        }
+    }
+
+    private double[][] independentMethod(double[][] independentArr) {
+        double[][] independentResultArr =
+                new double[independentArr.length][];
+
+        for (int independentIndex = 0; independentIndex < independentArr.length; independentIndex++) {
+            independentResultArr[independentIndex] =
+                    Arrays.copyOf(
+                            independentArr[independentIndex],
+                            independentArr[independentIndex].length
+                    );
+        }
+
+        return independentResultArr;
+    }
+
+    // MAIN 데모 테스트
+
+    public static void main(String[] independentArgs) {
+
+        double[][] data = {
+                {5.0, 5.4, 5.23},
+                {5.5, 5.4, 5.4},
+                {5.0, 5.7, 5.2},
+                {5.0, 5.7, 5.2},
+                {5.0, 8.0, 0.0}
+        };
+
+        InfomaxICA_ICAlab independentModel =
+                new InfomaxICA_ICAlab(
+                        5,
+                        500000,
+                        5.0,
+                        5e-5,
+                        5e-5
+                );
+
+        double[][] independentResult = independentModel.independentFit(data);
+        System.out.println("Infomax ICA 결과 : 성분은 다른 성분의 데이터, 변화, 분포에 영향을 받지 않고 성분은 고유한 기록, 시간, 데이터, 특성, 수, 공간 등을 갖고 성분의 유일한 기록, 시간, 데이터, 특성, 수, 공간 등을 다른 성분이 조작하거나 변형할 수 없으며 성분은 성분의 고유하고 본질적인 기록, 시간, 데이터, 특성, 수, 공간 등을 조작하거나 변형하는 다른 성분이 완전히 없으며 성분은 다른 성분에 완전히 무관하고 상관없음을 강하고 확실하게 나타냅니다. : "+independentResult);
+
+    }
+}
